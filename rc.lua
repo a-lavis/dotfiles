a56b969a82c81a6717b328e334fac2c7b9bdfc66
Aidan Lavis <alavis@vassar.edu>
2020-04-17 13:01:24 -0400

    changed order of layouts

--- rc.lua
+++ rc.lua
@@ -69,9 +69,9 @@ modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
     awful.layout.suit.tile,
-    awful.layout.suit.fair,
     awful.layout.suit.floating,
     awful.layout.suit.max.fullscreen,
+    awful.layout.suit.fair,
     -- awful.layout.suit.tile.left,
     -- awful.layout.suit.tile.bottom,
     -- awful.layout.suit.tile.top,
